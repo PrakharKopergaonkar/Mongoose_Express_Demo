b0a4e1f7e661e72e1ccfba9c0ee8705c143fcd88
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2021-03-17 21:14:12 +0530

    mongodb connection made

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const  app = express();
-const password = require("./password")["password"]
+const password = require("./password")
+const mongoose = require("mongoose");
+const Blog = require("./models/blog")
+
+//connect to mongodb
+const dbURI =   `mongodb+srv://Prakhar04:${password}@nodeclustur.urejm.mongodb.net/node-tutorial?retryWrites=true&w=majority`
+mongoose.connect(dbURI, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then((result) => {
+        console.log(result);
+        console.log(`Connected to db`)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
-const dbURI =   `mongodb+srv://Prakhar04:${password}@nodeclustur.urejm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -12,3 +24,22 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('app listening on port 3000!');
 });
+
+
+//mongoose and mongo routes
+
+app.get("/add-blog", (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    }); 
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            res.send(error.message)
+        });
+})
